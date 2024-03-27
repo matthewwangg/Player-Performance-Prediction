18a84638417a3d651d5e02fe050c1e227299780e
Matt Wang <matthewianwang@gmail.com>
2024-03-26 20:37:10 -0700

    Added data pipeline with FPL API

---
+++ web app/data_pipeline.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import pandas as pd
+from data_processing import find_path
+
+nametoid = {}
+
+# Function to gather data for a specific player
+def gather_player_data(player_id):
+    base_url = f"https://fantasy.premierleague.com/api/element-summary/{player_id}/"
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        player_data = response.json()
+        return player_data
+    else:
+        print(f"Failed to retrieve data for the player. Status code: {response.status_code}")
+        return None
+
+# Function to gather data for all players for a specific gameweek
+def gather_gameweek_data(gameweek):
+    base_url = f"https://fantasy.premierleague.com/api/event/{gameweek}/live/"
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        gameweek_data = response.json()
+        return gameweek_data
+    else:
+        print(f"Failed to retrieve data for gameweek {gameweek}. Status code: {response.status_code}")
+        return None
+
+def initialize_map():
+
+    df = pd.read_csv(find_path())
+
+    # Select only 'player_id' and 'player_name' columns
+    player_names_and_ids_df = df[['id', 'name']]
+    nametoid = player_names_and_ids_df.set_index('name')['id'].to_dict()
+
+    print(nametoid.keys())
+
